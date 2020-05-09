29ca87f65e5e26b103f04d70c76e49dcc9360723
initiuminc1 <initiuminc1@gmail.com>
2020-05-09 22:35:52 +0530

    EnableGlobalMethodSecurity

--- src/main/java/com/demo/jwt/config/SecurityConfiguration.java
+++ src/main/java/com/demo/jwt/config/SecurityConfiguration.java
@@ -1,10 +1,10 @@
 package com.demo.jwt.config;
 
-import com.demo.jwt.service.CustomUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -14,6 +14,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Autowired
