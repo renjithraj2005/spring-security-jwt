972279d76baf758134adf7aad904c2fba64f947a
initiuminc1 <initiuminc1@gmail.com>
2020-05-09 21:50:26 +0530

    models added

---
+++ src/main/java/com/demo/jwt/model/Role.java
@@ -0,0 +1,12 @@
+package com.demo.jwt.model;
+
+import org.springframework.security.core.GrantedAuthority;
+
+public enum Role  implements GrantedAuthority {
+    ROLE_ADMIN, ROLE_CLIENT;
+
+    @Override
+    public String getAuthority() {
+        return null;
+    }
+}
